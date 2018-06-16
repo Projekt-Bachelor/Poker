13fdf9a6b67938abbfcd9b501df3e34f6f364b64
Unknown <nikel0505@gmail.com>
2018-06-16 14:25:03 +0200

    Change Websocket
    Players can now "log in" on the start page. This process is now implemented via a Websocket instead of a Model-View-Controller.

--- src/main/java/de/tu_clausthal/in/bachelorproject2018/poker/controller/CPlayerController.java
+++ src/main/java/de/tu_clausthal/in/bachelorproject2018/poker/controller/CPlayerController.java
@@ -1,4 +1,19 @@
 package de.tu_clausthal.in.bachelorproject2018.poker.controller;
 
+import de.tu_clausthal.in.bachelorproject2018.poker.Network_Objects.Player;
+import de.tu_clausthal.in.bachelorproject2018.poker.game.player.CPlayer;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
 public class CPlayerController {
+
+    @MessageMapping("/createPlayer")
+    @SendTo("/topic/tables")
+    public void createPlayer(Player player) throws Exception {
+        CPlayer newPlayer = new CPlayer(player.getName());
+    }
+
 }
