4f76ca907575f1d3870bb1351d07844164b4d1a3
Unknown <nikel0505@gmail.com>
2018-06-18 17:19:47 +0200

    Adds the Java-Script-Files

---
+++ src/main/resources/static/game.js
@@ -0,0 +1,50 @@
+var stompClient = null;
+
+function setConnected(connected){
+    $("#connect").prop("disabled", connected);
+    $("disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#gamestate").show();
+    } else {
+        $("#gamestate").hide();
+    }
+    $("#gamestate").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/poker');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/game', function (gameinformation) {
+            showGameInformation(JSON.parse(gameinformation.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null){
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendRaise() {
+    stompClient.send("/app/gameAction/raise", {},
+        JSON.stringify({'value': $("#raise-amount").val()}));
+}
+
+function showGameInformation(gameinformation){
+    $("#gamestate").append("<tr><td>" + gameinformation + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#raise" ).click(function () { sendRaise(); });
+    $( "#connect").click(function () { connect(); });
+    $( "#disconnect").click(function () { disconnect(); });
+});
