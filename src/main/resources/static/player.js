9090e901de7789f3cfecf5a323e9f1e1fc16d637
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-06-19 15:14:42 +0200

    UI

--- src/main/resources/static/player.js
+++ src/main/resources/static/player.js
@@ -1,3 +1,64 @@
+// erzwingnt sauberen JavaScript Code
+"use strict";
+
+// wenn HTML Dokument komplett geladen wurde
+$(function() {
+
+    // führe einen Ajax Request aus
+    jQuery.ajax( "/tables/list" )
+
+        // wenn HTTP 200, dann führe Funktion aus
+        .done(function(i) {
+
+            // es kommt ein Array und für jedes Element
+            i.forEach( function(n) {
+
+                // Spiele-DOM-Objekt holen
+                $("#spiele").append(
+                    // li-Objekt anhängen
+                    $("<li/>").append(
+                        // in li ein a-Objekt anhängen
+                        $("<a/>")
+                            // dort Text rein setzen
+                            .text(n)
+                            // Klasse hinzufügen
+                            .addClass("spieljoin")
+                            // href Attribut setzen
+                            .attr("href", "#" )
+                            // Data-Attribut setzen
+                            .attr("data-name", n)
+                    )
+                )}
+            );
+        })
+
+        // wenn HTTP != 200
+        .fail(function(i) {
+            console.log(i);
+            // Fehlermeldung
+            alert("Fehler");
+        });
+
+        // binde an alle .spieljoin Objekte
+        $("body").on("click", ".spieljoin", function() {
+
+            // führe Ajax Request aus und hole Daten aus dem Data-Attribut des a-Objekts und dem Input-Feld
+            jQuery.ajax( "/table/join/" + jQuery(this).data("name") + "/" + $("#name").val()  )
+                .done(function(i) {
+                    console.log(i);
+                })
+                .fail(function(i) {
+                    console.log(i);
+                })
+        });
+
+});
+
+
+
+
+
+
 let stompClient = null;
 
 let successResponse = 100;
@@ -14,7 +75,7 @@ function setConnected(connected) {
     }
     $("#greetings").html("");
 }
-
+/*
 function requestJSON(url, callback) {
     fetch(url)
         .then(response => response.json())
@@ -22,7 +83,7 @@ function requestJSON(url, callback) {
             callback(data)
         }).catch(error => console.error('Could not fetch url: ', error))
 }
-
+*/
 function connect() {
     var socket = new SockJS('/poker');
     stompClient = Stomp.over(socket);
@@ -48,17 +109,11 @@ function sendName() {
         JSON.stringify({'name': $("#name").val()}));
 }
 
-function getTables() {
-    requestJSON("/tables/list", showTables())
-}
 
 function showTables(tables) {
     tables.forEach(display)
 }
 
-function showTable(tableName) {
-    $("#tables").append("<tr><td>" + tableName + "</td></tr>");
-}
 
 function validateResponse(response){
     //TODO - Abfrage auf Success-Meldung implementieren
